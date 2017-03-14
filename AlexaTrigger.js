c0e7fbe5bc41fa31d1d17a680f7725c12637afd8
princedc <prince.devasitham@gmail.com>
2017-03-14 06:37:07 +0530

    First Commit

---
+++ AlexaTrigger.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+
+var port = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+  console.log('Invoked Function')
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
+})
